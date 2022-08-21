85ed01aa4655b06723a5ac46035563708d9e81eb
y22ming <olivia.ong22@gmail.com>
2022-08-21 22:02:38 +0800

    Testing the API

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,160 +1,87 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet, TouchableHighlight} from 'react-native';
-import {FlatList} from 'react-native-gesture-handler';
-import {FloatingAction} from 'react-native-floating-action';
+import React, { Component } from "react";
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  FlatList,
+  View,
+  Switch,
+  Button,
+  TouchableNativeFeedback,
+  Alert,
+} from "react-native";
+import { InputWithLabel, PickerWithLabel, AppButton } from "../UI";
 
-
-// not done
-let common = require('../CommonData');
-let SQLite = require('react-native-sqlite-storage');
-
-const actions = [
-  {
-    text: 'Add',
-    icon: require('../assets/icons/add.png'),
-    name: 'add',
-    position: 1,
-  },
-];
+let config = require("../Config");
 
 export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      students: [],
+      members: [],
+      isFetching: false,
     };
-    this._query = this._query.bind(this);
-    this._databasePrepare = this._databasePrepare.bind(this);
-    this.db = SQLite.openDatabase(
-      {name: 'studentdb2'},
-      this.openCallback,
-      this.errorCallback,
-    );
+    this._load = this._load.bind(this);
   }
+
+  _load() {
+    let url = config.settings.serverPath + "/api/member";
+    this.setState({ isFetching: true });
+    fetch(url)
+      .then((response) => {
+        console.log(response);
+        if (!response.ok) {
+          Alert.alert("Error:", response.status.toString());
+          throw Error("Error " + response.status);
+        }
+        this.setState({ isFetching: false });
+        return response.json();
+      })
+      .then((members) => {
+        console.log(members);
+        this.setState({ members: members });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   componentDidMount() {
-    this._databasePrepare();
-    this._query();
-  }
-  _databasePrepare() {
-    this.db.transaction(tx => {
-      tx.executeSql(
-        'CREATE TABLE IF NOT EXISTS students(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(20), email VARCHAR(20), state VARCHAR(20))',
-        [],
-        (sqlTxn, res) => {
-          console.log('students table ready');
-        },
-        error => {
-          console.log('error on creating table ' + error.message);
-        },
-      );
-      this.db.transaction(tx =>
-        tx.executeSql(
-          'SELECT * FROM students ORDER BY name',
-          [],
-          (tx, results) => {
-            if (results.rows.length == 0) {
-              tx.executeSql(
-                'INSERT INTO students(name,email,state) VALUES("Chia Kim Hooi","chiakh@duckmail.com","07")',
-                [], //insert dummy data so that the database is non-empty, to ease verification
-                (tx, results) => {
-                  if (results.rowsAffected > 0) {
-                    console.log('dummy data inserted successfully');
-                    this._query();
-                  } else {
-                    console.log('error in inserting data');
-                  }
-                },
-              );
-            } else {
-              console.log('table non-empty, no insertion needed');
-            }
-          },
-        ),
-      );
-    });
+    this._load();
   }
 
-  _query() {
-    this.db.transaction(tx =>
-      tx.executeSql('SELECT * FROM students ORDER BY name', [], (tx, results) =>
-        this.setState({students: results.rows.raw()}),
-      ),
-    );
-  }
-
-  openCallback() {
-    console.log('database open success');
-  }
-  errorCallback(err) {
-    console.log('Error in opening the database: ' + err);
-  }
   render() {
-    console.log(this.state.students);
     return (
-      <View style={styles.container}>
+      <View style={{ flex: 1, margin: 5 }}>
         <FlatList
-          data={this.state.students}
-          extraData={this.state}
-          showsVerticalScrollIndicator={true}
-          renderItem={({item}) => (
-            <TouchableHighlight
-              underlayColor="pink"
-              onPress={() => {
-                this.props.navigation.navigate('ViewScreen', {
-                  id: item.id,
-                  headerTitle: item.name,
-                  refresh: this._query,
-                });
-              }}>
-              <View style={styles.item}>
-                <Text style={styles.itemTitle}>{item.name}</Text>
-                <Text style={styles.itemSubtitle}>
-                  {common.getValue(common.states, item.state)}
-                </Text>
-              </View>
-            </TouchableHighlight>
-          )}
-          keyExtractor={item => {
-            item.id.toString();
+          refreshing={this.state.isFetching}
+          onRefresh={this._load}
+          data={this.state.members}
+          renderItem={({ item }) => {
+            return (
+              <TouchableNativeFeedback
+                onPress={() =>
+                  this.props.navigation.navigate("View", {
+                    id: item.id,
+                    _refresh: this._load,
+                  })
+                }
+              >
+                <View
+                  style={{ borderBottomWidth: 1, borderBottomColor: "grey" }}
+                >
+                  <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+                    {item.name}
+                  </Text>
+                  <Text style={{ fontSize: 15 }}>{item.email}</Text>
+                  <Text style={{ fontSize: 15 }}>{item.phone}</Text>
+                </View>
+              </TouchableNativeFeedback>
+            );
           }}
-        />
-        <FloatingAction
-          actions={actions}
-          overrideWithAction={true}
-          color={'#a80000'}
-          onPressItem={() => {
-            this.props.navigation.navigate('CreateScreen', {
-              refresh: this._query,
-            });
-          }}
-        />
+        ></FlatList>
       </View>
     );
   }
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    backgroundColor: '#fff',
-  },
-  item: {
-    justifyContent: 'center',
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 25,
-    paddingRight: 25,
-    borderBottomWidth: 1,
-    borderColor: '#ccc',
-  },
-  itemTitle: {
-    fontSize: 22,
-    fontWeight: '500',
-    color: '#000',
-  },
-  itemSubtitle: {
-    fontSize: 18,
-  },
-});
